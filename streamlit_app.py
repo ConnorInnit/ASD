dab7956f52ea2546f1a52304f4b756a55a73facf
Nuff <62507982+ConnorInnit@users.noreply.github.com>
2023-09-05 15:56:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,9 @@ st.sidebar.title("Wall Dimensions")
 height = st.sidebar.number_input("Height (meters):", min_value=0.1, value=1.0, step=0.1)
 length = st.sidebar.number_input("Length (meters):", min_value=1.0, value=10.0, step=1.0)
 
+st.image("https://github.com/ConnorInnit/ASD/raw/main/assets/62507982/662debea-d3f4-448c-bf68-7ffda2db04a2.jpg", 
+          caption="Card Logo", use_column_width=True)
+
 # Calculate values
 stone_tonnage = calculate_stone_tonnage(length, height)
 stone_cost = estimate_stone_cost(stone_tonnage)
