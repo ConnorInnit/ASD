4bb56434a42bdc9e2721b95aabfa2d05d15b2b31
Nuff <62507982+ConnorInnit@users.noreply.github.com>
2023-09-05 15:10:47 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,44 @@
+import streamlit as st
+
 # Constants based on research (replace with actual values)
-STONE_COST_PER_TON = 189  #Cost per ton provided from research
-LABOR_COST_PER_SQUARE_METER = 200  #Cost per square metre provided by research
-LABOR_COST_PER_HOUR = 32  #cost per hour provided from Research
+STONE_COST_PER_TON = 189  # Replace with the actual cost per ton of stone
+LABOR_COST_PER_SQUARE_METER = 200  # £200 per square meter
+LABOR_COST_PER_HOUR = 25  # £25 per hour
 
 # Function to calculate tonnage of stone required
 def calculate_stone_tonnage(length, height):
-    # Calculate the area of the wall (length times height
+    # Calculate the area of the wall (length times height)
     wall_area = length * height
     # Research shows the density would be 1.6, therefore we assign stone density the value of 1.6 (ton)
     stone_density = 1.6
-    # Calculate and assign the tonnage using multiplication of area and density, and convert to metric tonnes
+    # Calculate and assign the tonnage using multiplication of area and density, and convert to metric tons (tonnes).
     stone_tonnage = wall_area * stone_density
     return stone_tonnage
 
-# Function to calculate the cost of stone
+# Function to estimate the cost of stone
 def estimate_stone_cost(tonnage):
-    #Used for getting stone cost when calculating values (calls stone_tonnage, essentially replacing tonnage with stone_tonnage, which is then multiplied by STONE_COST_PER_TON
     return tonnage * STONE_COST_PER_TON
 
-# Function to calculate the cost of labor based on square meter rate
+# Function to estimate the cost of labor based on square meter rate
 def estimate_labor_cost_per_square_meter(length, height):
     # Calculate the labor cost based on the wall area and the provided labor rate
     wall_area = length * height
-    #assigns labor_cost to wall area multiplied by cost per square metre
     labor_cost = wall_area * LABOR_COST_PER_SQUARE_METER
     return labor_cost
 
-# Function to calculate the cost of labor based on hourly rate
+# Function to estimate the cost of labor based on hourly rate
 def estimate_labor_cost_per_hour(length, height):
     # Calculate the total labor hours required for the wall
     labor_hours = length * height * 8  # Assuming 8 hours of labor per square meter
     labor_cost = labor_hours * LABOR_COST_PER_HOUR
     return labor_cost
 
+# Streamlit app
+st.title("Dry Stone Wall Calculator")
+
 # User inputs
-height = float(input("Enter the height of the wall in meters: "))
-length = float(input("Enter the length of the wall in meters: "))
+height = st.number_input("Enter the height of the wall in meters:")
+length = st.number_input("Enter the length of the wall in meters:")
 
 # Calculate values
 stone_tonnage = calculate_stone_tonnage(length, height)
@@ -44,8 +47,7 @@ labor_cost_per_square_meter = estimate_labor_cost_per_square_meter(length, heigh
 labor_cost_per_hour = estimate_labor_cost_per_hour(length, height)
 
 # Display the results
-print(f"Tonnage of stone required: {stone_tonnage:.2f} tonnes")
-print(f"Estimated cost of stone: £{stone_cost:.2f}")
-print(f"Estimated cost of labor per square meter: £{labor_cost_per_square_meter:.2f}")
-print(f"Estimated cost of labor per hour: £{labor_cost_per_hour:.2f}")
-
+st.write(f"Tonnage of stone required: {stone_tonnage:.2f} tonnes")
+st.write(f"Estimated cost of stone: £{stone_cost:.2f}")
+st.write(f"Estimated cost of labor per square meter: £{labor_cost_per_square_meter:.2f}")
+st.write(f"Estimated cost of labor per hour: £{labor_cost_per_hour:.2f}")
