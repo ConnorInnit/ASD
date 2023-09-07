14c59630ceaf10dfbfba20db32eff05c3fd4a23b
Nuff <62507982+ConnorInnit@users.noreply.github.com>
2023-09-07 13:35:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ st.set_page_config(
 
 st.title("Dry Stone Wall Calculator")
 st.header("Estimate the Cost of Building a Dry Stone Wall")
-if not 'Wall Dimensions' == 'collapsed':
+if not 'Wall Dimensions' == 'expanded':
     st.write("Please click the top left arrow to enter your desired wall dimensions")
 
 # Sidebar with user inputs
